f202a29fec4960f6f1ec6ba32b4a7d408a2ca967
Af-ansah <ansahgloria032@gmail.com>
2024-05-30 14:27:02 +0000

    Rest countries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { SearchComponent } from "./shared/search/search.component";
+import { CardComponent } from "./shared/card/card.component";
+import { DataService } from './data.service';
+import { CountryInfo } from './interfaces/countries.interface';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavbarComponent, SearchComponent, CardComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rest-countries';
+
+
+  private readonly dataService = inject(DataService)
+  countries: CountryInfo[] = [];
+
+  ngOnInit(){
+
+    this.dataService.getCountries().subscribe({
+      next: (response) => {
+        this.countries = response;
+        console.log(response)
+      }
+    })
+  }
+
+  trackByCountryId(index: number, country: CountryInfo): string {
+    return country.id.cca3;
+  }
 }
